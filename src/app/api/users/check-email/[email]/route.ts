b6ece86feb5c8fb977195aa69aa58f3ee88cb599
CoviloMilos <miloscovilons@gmail.com>
2024-12-27 23:37:52 +0100

    remove firebase

--- src/app/api/users/check-email/[email]/route.ts
+++ src/app/api/users/check-email/[email]/route.ts
@@ -1,24 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
-import { checkEmailInUse } from "@/lib/firebase-admin";
-import { object, string, ValidationError } from "yup";
+import { NextResponse } from "next/server";
 
-export async function GET(_: NextRequest, context: { params: Promise<{ email: string }> }) {
+export async function GET() {
   try {
-    const { email } = (await context.params) as { email: string };
-
-    await emailCheckSchema.validate({ email }, { abortEarly: false });
-
-    const inuse = await checkEmailInUse(email);
-
-    return NextResponse.json({ inuse }, { status: 200 });
+    return NextResponse.json({ inuse: true }, { status: 200 });
   } catch (error) {
-    if (error instanceof ValidationError) {
-      return NextResponse.json({ errors: error.errors }, { status: 400 });
-    }
+    console.error(error);
     return NextResponse.json({ message: "An unexpected error occurred" }, { status: 500 });
   }
 }
-
-const emailCheckSchema = object({
-  email: string().trim().email("Invalid email format").required("Email is required"),
-});
